e3a7d6386b8086eae79ef798fda185c4b14b5ebc
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-04-10 21:12:20 +0300

    HomeWork

---
+++ Hospital/Doctor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seventh_lesson.Hospital
+{
+    public class Doctor
+    {
+        public Doctor() { }
+        public Doctor(string DoctorName)
+        {
+            this.DoctorName = DoctorName;
+        }
+        public string DoctorName { get; set; }
+
+        public void Treat(string whoWillTreatYou)
+        {
+            switch (whoWillTreatYou)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Хирург режит");
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Дантиста пилит");
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Терапевта лечит");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("");
+                        break;
+                    }
+            }
+        }
+
+        public void WhoWillTreatYou(TreatmentPlan treatmentPlan)
+        {
+
+            switch (treatmentPlan.Plan)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Назначить хирурга");
+                        Treat("1");
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Назначить дантиста");
+                        Treat("2");
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Назначить терапевта");
+                        Treat("3");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("");
+                        break;
+                    }
+            }
+
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seventh_lesson
+{
+    public class Square
+    {
+        public Square() { }
+
+        public Square(int Sidelength, string Color)
+        {
+            this.Sidelength = Sidelength;
+            this.Color = Color;
+            
+        }
+
+        public int Sidelength { get; set; }
+        public string Color { get; set; }
+        public int Area => CalculatesTheAreaOfAFigure();
+
+        public override string ToString()
+        {
+            return $"{Sidelength} - Длина стороны, {Color} - Цвет, {Area} - Область";
+        }
+
+        private int CalculatesTheAreaOfAFigure()
+        {
+           
+           
+           return Sidelength * Sidelength;
+        }
+
+    }
+}
